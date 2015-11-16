eabc5832fa0bccd3fb38ca08209e27eff134056a
Edwin Guzman <edwin.gzmn@gmail.com>
2015-11-16 16:33:15 -0500

    First coomit for alt repo.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+var nyplModule;
+
+function exampleFun(param) {
+  return param;
+};
+
+nyplModule = {
+  exampleFun: exampleFun,
+  exampleVar: 'This is an example variable'
+};
+
+module.exports = nyplModule;
